06aef5e09a3cfa56a24957175df49fb06bc8bfac
toma <144379705+SnowDrop23@users.noreply.github.com>
2025-05-17 21:27:15 +0600

    Update (LeetCode) 20. Valid Parentheses or (HackerEarth) Balanced Brackets.cpp

--- Stack Question Solved/(LeetCode) 20. Valid Parentheses or (HackerEarth) Balanced Brackets.cpp
+++ Stack Question Solved/(LeetCode) 20. Valid Parentheses or (HackerEarth) Balanced Brackets.cpp
@@ -6,6 +6,9 @@
 #include <stack>
 using namespace std;
 
+
+// Map to represent bracket types with unique values for matching
+// Opening brackets have negative values, closing have positive values
 unordered_map<char, int> symbols = {{'[', -1}, {'{', -2}, {'(', -3}, {']', 1}, {'}', 2}, {')', 3}};
 
 bool isBalanced(string s)
@@ -13,17 +16,24 @@ bool isBalanced(string s)
     stack<char> st;
     for(char &bracket : s)
     {
+
+        // If opening bracket, push onto the stack
         if(symbols[bracket] < 0) st.push(bracket);
 
         else
         {
+
             if(st.empty()) return false;
-            
+
+            // Pop top element from the stack
             char top = st.top();
             st.pop();
+
+            // Check if the popped opening bracket matches the closing bracket
             if(symbols[top] + symbols[bracket] != 0) return false;
         }
     }
+    // If stack is empty, all brackets matched
     if(st.empty()) return true;
     return false;
 
@@ -33,13 +43,15 @@ bool isBalanced(string s)
 int main ()
 {
     int t;
+    cout << "Enter number of test cases: ";
     cin >> t;
     while(t--)
     {
         string str;
+        cout << "Enter bracket string: ";
         cin >> str;
 
-        
+        // Output result based on whether the string is balanced
         if(isBalanced(str)) cout << "YES\n";
         else cout << "NO\n";
     }
